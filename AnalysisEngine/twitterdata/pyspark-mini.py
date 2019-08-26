bb9a35965461baaeba9f5765637a7dfc61f22513
rohithjoshy34@gmail.com <rohithjoshy34@gmail.com>
2019-08-26 13:58:08 +0530

    commit 26-august

--- AnalysisEngine/twitterdata/pyspark-mini.py
+++ AnalysisEngine/twitterdata/pyspark-mini.py
@@ -1,30 +1,60 @@
 from pyspark.sql import SQLContext
 from pyspark import SparkContext, SparkConf
 from pyspark.sql.functions import *
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, CountVectorizer
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml import Pipeline
+
 
 sc =SparkContext()
 sqlContext = SQLContext(sc)
 
-df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('tweets.csv')
-newDf = df.withColumn('Tweets', regexp_replace('Tweets', '[^a-zA-Z0-9]', ' '))
-print(newDF)
+dfdata = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('tweets.csv')
+df = dfdata.withColumn('Tweets', regexp_replace('Tweets', '[^a-zA-Z0-9]', ' '))
+df.show(5)	
+(train_set, val_set, test_set) = df.randomSplit([0.98, 0.01, 0.01], seed = 2000)
+df
+tokenizer = Tokenizer(inputCol="Tweets", outputCol="words")
+hashtf = HashingTF(numFeatures=2**16, inputCol="words", outputCol='tf')
+idf = IDF(inputCol='tf', outputCol="features", minDocFreq=5) #minDocFreq: remove sparse terms
+'''label_stringIdx = StringIndexer(inputCol = "target", outputCol = "label")'''
+pipeline = Pipeline(stages=[tokenizer, hashtf, idf])
 
-'''from pyspark import SparkContext
-from pyspark.sql import SQLContext
-import pandas as pd
+pipelineFit = pipeline.fit(train_set)
+train_df = pipelineFit.transform(train_set)
+val_df = pipelineFit.transform(val_set)
+train_df.show(5)	
 
-sc = SparkContext('local','example')  # if using locally
-sql_sc = SQLContext(sc)
 
-pandas_df = pd.read_csv('tweets.csv')  # assuming the file contains a header
-# pandas_df = pd.read_csv('file.csv', names = ['column 1','column 2']) # if no header
-s_df = sql_sc.createDataFrame(pandas_df)
-print(s_df)'''
+from pyspark.ml.classification import LogisticRegression
+lr = LogisticRegression(maxIter=100)
+lrModel = lr.fit(train_df)
+predictions = lrModel.transform(val_df)
 
-'''from pyspark import SparkContext, SparkConf
-import csv
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction")
+evaluator.evaluate(predictions)
 
-sc =SparkContext()
-rdd = sc.textFile("tweets.csv")
-rdd = rdd.mapPartitions(lambda x: csv.reader(x))
-print(rdd)'''
+from pyspark.ml.feature import CountVectorizer
+
+tokenizer = Tokenizer(inputCol="text", outputCol="words")
+cv = CountVectorizer(vocabSize=2**16, inputCol="words", outputCol='cv')
+idf = IDF(inputCol='cv', outputCol="features", minDocFreq=5) #minDocFreq: remove sparse terms
+label_stringIdx = StringIndexer(inputCol = "target", outputCol = "label")
+lr = LogisticRegression(maxIter=100)
+pipeline = Pipeline(stages=[tokenizer, cv, idf, label_stringIdx, lr])
+
+pipelineFit = pipeline.fit(train_set)
+predictions = pipelineFit.transform(val_set)
+accuracy = predictions.filter(predictions.label == predictions.prediction).count() /float(val_set.count())
+roc_auc = evaluator.evaluate(predictions)
+
+print "Accuracy Score: {0:.4f}".format(accuracy)
+print "ROC-AUC: {0:.4f}".format(roc_auc)
+
+
